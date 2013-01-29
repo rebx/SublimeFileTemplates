3253f42b4425622f88aee668cc4939fca7edae7b
Rebs Guarina <rebs.guarina@gmail.com>
2013-01-29 16:12:01 +1300

    Added ruby and perl templates _para mi_

---
+++ Templates/Perl.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Pod::Usage;
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+
+sub main {
+
+	return 1;
+}
+
+main() and exit 0;
+exit 2;
