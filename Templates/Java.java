ea96cabe3d226937f92f2751eca86732d5c4f132
Rebs Guarina <rebs.guarina@gmail.com>
2013-01-30 15:10:55 +1300

    Added java templates

---
+++ Templates/Java.java
@@ -0,0 +1,26 @@
+
+/**
+ *
+ *
+ */
+package $name;
+
+/**
+ *
+ * @author
+ * @version
+ *
+ */
+public class $name {
+
+	/**
+	 *
+	 * @param args 
+	 * @return
+	 * @throws
+	 * @see
+	 */
+	public static void main(String[] args) {
+
+	}
+}
