469e6f110f0c6eb72d2a1a7f57aa372a8bf837f3
Rebs Guarina <rebs.guarina@gmail.com>
2013-01-30 15:08:52 +1300

    Added Author, Description, Usage lines

--- Templates/Perl.pl
+++ Templates/Perl.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/env perl
 
+###############################################################################
+# Author:
+#
+# Description:
+# 
+# Usages:
+#
+###############################################################################
+
 use strict;
 use warnings;
 
@@ -16,3 +25,35 @@ sub main {
 
 main() and exit 0;
 exit 2;
+
+__END__
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Show this help dialog window
+
+=back
+
+=head1 USAGE EXAMPLES
+
+=over 8
+
+=back
+
+=head1 SEE ALSO
+
+=over 8
+
+=back
+
+=cut
--- Templates/Ruby.rb
+++ Templates/Ruby.rb
@@ -1,4 +1,34 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 
+###############################################################################
+# Author:
+#
+# Description:
+# 
+# Usages:
+#
+###############################################################################
+
+begin
+	if RbConfig::CONFIG["ruby_version"].to_s.to_f < 1.9
+		require 'rubygems'
+	end
+rescue LoadError
+	warn "Failed to load rubygems for ruby interpreter version " + \
+		RbConfig::CONFIG["ruby_version"]
+end
+
 \$:.unshift File.join(File.dirname(__FILE__), 'lib')
+
+# An example function that returns anything passed to it
+# 
+# == Parameters:
+# bar::
+#    Any kind of variable
+# 
+# == Returns:
+# The variable passed to this function
+def foo(bar)
+	bar
+end
